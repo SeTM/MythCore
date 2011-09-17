726c980b2c26e02b7c683419ebacc9e6331d5a74
finomen <finomen@kt15.ru>
2011-09-17 20:32:04 +0400

    Bonus vendor gossip messages

--- src/server/scripts/Custom/npc_bonuses.cpp
+++ src/server/scripts/Custom/npc_bonuses.cpp
@@ -1,21 +1,19 @@
 #include "Config.h"
 
-#define COST_RACE 3000
-#define COST_FACTION 4000
-#define COST_RENAME 1000
-#define COST_GOLD 500
+#define COST_RACE 5000
+#define COST_RENAME 5000
+#define COST_GOLD 30
 
 #define STRING(x) #x
 #define STR(x) STRING(x)
 #define GOSSIP_HELLO             "Vote bonuses system"
-#define GOSSIP_RACE              "Change race (" STR(COST_RACE) ")"
-#define GOSSIP_FACTION           "Change faction (" STR(COST_GENDER) ")"
-#define GOSSIP_RENAME            "Rename (" STR(COST_RENAME) ")"
-#define GOSSIP_NOMONEY           "You need more votes"
-#define GOSSIP_SUCCESS           "Will be changed on nex login"
-#define GOSSIP_SUCC_BUY          "Success"
-#define GOSSIP_SHOP              "Bonus shop"
-#define GOSSIP_GOLD              "1000 gold (" STR(COST_GENDER) ")"
+#define GOSSIP_RACE              "Сменить расу (" STR(COST_RACE) " золота)"
+#define GOSSIP_RENAME            "Переименовать персонажа (" STR(COST_RENAME) " золота)"
+#define GOSSIP_NOMONEY           "Недостаточно средств"
+#define GOSSIP_SUCCESS           "Изменения вступят в силу при следующем входе в игру"
+#define GOSSIP_SUCC_BUY          "Операция прошла успешно"
+#define GOSSIP_SHOP              "Обмен голосов"
+#define GOSSIP_GOLD              "1000 золота (" STR(COST_GOLD) " голосов)"
 
 struct item {
     std::string name;
@@ -116,8 +114,6 @@ public:
                             GOSSIP_SENDER_MAIN, ACTION_FACTION);*/
         pPlayer->ADD_GOSSIP_ITEM(ICON_GOSSIP_GOLD, GOSSIP_RENAME,
                             GOSSIP_SENDER_MAIN, ACTION_RENAME);
-        pPlayer->ADD_GOSSIP_ITEM(ICON_GOSSIP_GOLD, GOSSIP_GOLD,
-                                    GOSSIP_SENDER_MAIN, ACTION_GOLD);
         pPlayer->ADD_GOSSIP_ITEM(ICON_GOSSIP_BALOON, GOSSIP_SHOP,
                                     GOSSIP_SENDER_MAIN, ACTION_SHOP);
         pPlayer->SEND_GOSSIP_MENU(DEFAULT_GOSSIP_MESSAGE, pCreature->GetGUID());
@@ -139,8 +135,8 @@ public:
         switch (uiAction)
         {
         case ACTION_RACE:
-            if (i->GetCount() >= COST_RACE)
-                i->SetCount(i->GetCount() - COST_RACE);
+            if (pPlayer->GetMoney() >= COST_RACE)
+                pPlayer->SetMoney(pPlayer->GetMoney() - COST_RACE * 100 * 100);
             else
             {
                 pCreature->MonsterWhisper(GOSSIP_NOMONEY, pPlayer->GetGUID());
@@ -148,21 +144,11 @@ public:
             }
             pCreature->MonsterWhisper(GOSSIP_SUCCESS, pPlayer->GetGUID());
             pPlayer->SetAtLoginFlag(AT_LOGIN_CHANGE_RACE);
+            pPlayer->PlayerTalkClass->SendCloseGossip();
             return true;
-        /*case ACTION_FACTION:
-            if (i->GetCount() >= COST_FACTION)
-                i->SetCount(i->GetCount() - COST_FACTION);
-            else
-            {
-                pCreature->MonsterWhisper(GOSSIP_NOMONEY, pPlayer->GetGUID());
-                return false;
-            }
-            pCreature->MonsterWhisper(GOSSIP_SUCCESS, pPlayer->GetGUID());
-            pPlayer->SetAtLoginFlag(AT_LOGIN_CHANGE_FACTION);
-            return true;*/
         case ACTION_RENAME:
-            if (i->GetCount() >= COST_RENAME)
-                i->SetCount(i->GetCount() - COST_RENAME);
+            if (pPlayer->GetMoney() >= COST_RENAME)
+                pPlayer->SetMoney(pPlayer->GetMoney() - COST_RENAME * 100 * 100);
             else
             {
                 pCreature->MonsterWhisper(GOSSIP_NOMONEY, pPlayer->GetGUID());
@@ -170,6 +156,7 @@ public:
             }
             pCreature->MonsterWhisper(GOSSIP_SUCCESS, pPlayer->GetGUID());
             pPlayer->SetAtLoginFlag(AT_LOGIN_RENAME);
+            pPlayer->PlayerTalkClass->SendCloseGossip();
             return true;
         case ACTION_GOLD:
             if (i->GetCount() >= COST_GOLD)
@@ -181,6 +168,7 @@ public:
             }
             pCreature->MonsterWhisper(GOSSIP_SUCC_BUY, pPlayer->GetGUID());
             pPlayer->SetMoney(pPlayer->GetMoney() + 1000 * 100 * 100);
+            pPlayer->PlayerTalkClass->SendCloseGossip();
             return true;
         }
 
@@ -190,19 +178,15 @@ public:
 
             if (cat == 0)
             {
-                /*CURRENCY = 0,
-                    EQUIP = 1,
-                    MAUNT = 2,
-                    PET = 3,
-                    STONE = 4,
-                    EXCLUSIVE = 5,
-                    CAT_COUNT = 6*/
-                char catname[6][20] = {"Cash", "Equip", "Maunt", "Pet", "Gem", "Exclusive"};
+                char catname[6][20] = {"Валюта", "Одежда", "Маунты", "Спутники", "Камни", "Эксклюзив"};
                 for (int i = 1; i <= CAT_COUNT; ++i)
                 {
                     pPlayer->ADD_GOSSIP_ITEM(ICON_GOSSIP_BALOON, catname[i - 1],
                                             GOSSIP_SENDER_MAIN, ACTION_SHOP | i);
                 }
+
+                pPlayer->ADD_GOSSIP_ITEM(ICON_GOSSIP_GOLD, GOSSIP_GOLD,
+                    GOSSIP_SENDER_MAIN, ACTION_GOLD);
                 pPlayer->SEND_GOSSIP_MENU(DEFAULT_GOSSIP_MESSAGE, pCreature->GetGUID());
                 return true;
             }
@@ -215,7 +199,7 @@ public:
                     if (cati[i].lvl > pPlayer->getLevel())
                         continue;
                     char buf[1024];
-                    sprintf(buf, "%s [%d]", cati[i].name.c_str(), cati[i].cost);
+                    sprintf(buf, "%s (%d голосов)", cati[i].name.c_str(), cati[i].cost);
                     pPlayer->ADD_GOSSIP_ITEM(ICON_GOSSIP_BALOON, buf ,
                                 GOSSIP_SENDER_MAIN, ACTION_BUY | cat | (i << 5));
                 }
@@ -240,7 +224,7 @@ public:
 
             pCreature->MonsterWhisper(GOSSIP_SUCC_BUY, pPlayer->GetGUID());
             pPlayer->AddItem(it.id, 1);
-
+            pPlayer->PlayerTalkClass->SendCloseGossip();
         }
 
         return false;
